be8268a77e37c44110a27f3616806219027453e7
ShuaiShuaiDa8888 <601770050@qq.com>
2020-03-26 11:47:29 +0800

    将微服务注册到需认证的eureka server上

--- microserver-eureka-server/src/main/java/com/microserver/eurekaserver/EurekaServerApplication.java
+++ microserver-eureka-server/src/main/java/com/microserver/eurekaserver/EurekaServerApplication.java
@@ -2,10 +2,18 @@ package com.microserver.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-@SpringBootApplication
 @EnableEurekaServer
+@SpringBootApplication(exclude={
+        DataSourceAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class
+})
 public class EurekaServerApplication {
 
     public static void main(String[] args) {
@@ -13,4 +21,15 @@ public class EurekaServerApplication {
         System.out.println("----------EurekaServerApplication is started------------");
     }
 
+    //2.1版本的security默认加上了 csrf 拦截, 所以需要通过重写方法, 把csrf拦截禁用
+    @EnableWebSecurity
+    static class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.csrf().ignoringAntMatchers("/eureka/**");
+            super.configure(http);
+        }
+    }
+
 }
+
