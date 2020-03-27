6055f38bec162efbb286b1afbb8d186f107eff90
ShuaiShuaiDa8888 <601770050@qq.com>
2020-03-27 11:53:54 +0800

    实现eureka的元数据

--- microservice-movie/src/main/java/com/microservice/movie/controller/MovieController.java
+++ microservice-movie/src/main/java/com/microservice/movie/controller/MovieController.java
@@ -2,10 +2,14 @@ package com.microservice.movie.controller;
 
 import com.microservice.microservice.module.ResultInfo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 /**
  * @program: micro-service
@@ -18,6 +22,8 @@ public class MovieController {
 
     @Autowired
     private RestTemplate restTemplate;
+    @Autowired
+    private DiscoveryClient discoveryClient;
 
     private static final String FIND_BY_ID_URL = "http://127.0.0.1:8000/";
 
@@ -25,6 +31,18 @@ public class MovieController {
     public ResultInfo findById(@PathVariable long id){
         ResultInfo resultInfo = restTemplate.getForObject(FIND_BY_ID_URL + id, ResultInfo.class);
         return resultInfo;
+    }
+
+    /*** 
+    * @Description: 查询user服务的信息并返回
+    * @Param: [] 
+    * @return: java.util.List<org.springframework.cloud.client.ServiceInstance> 
+    * @Author: ws 
+    * @Date: 2020/3/27 
+    */
+    @GetMapping("/user_instance")
+    public List<ServiceInstance> showInfo(){
+        return discoveryClient.getInstances("microservice-user");
     }
 
 }
