31ab44d5bc2a3bb612765ecf34bbb9961fcbd059
ShuaiShuaiDa8888 <601770050@qq.com>
2020-03-30 11:31:40 +0800

    新建zuul项目

---
+++ microservice-zuul/src/test/java/com/microservice/microservicezuul/MicroserviceZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.microservicezuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceZuulApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
