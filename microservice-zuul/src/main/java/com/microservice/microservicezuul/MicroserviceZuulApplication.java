23b2078162e0d9c313f5ba82870788b5ace21196
ShuaiShuaiDa8888 <601770050@qq.com>
2020-03-30 15:26:39 +0800

    完成方大佬的路由转发

--- microservice-zuul/src/main/java/com/microservice/microservicezuul/MicroserviceZuulApplication.java
+++ microservice-zuul/src/main/java/com/microservice/microservicezuul/MicroserviceZuulApplication.java
@@ -2,12 +2,17 @@ package com.microservice.microservicezuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableZuulProxy
 public class MicroserviceZuulApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MicroserviceZuulApplication.class, args);
+        System.out.println("---------zuul is started-----------");
     }
 
 }
---
+++ port.md
@@ -0,0 +1,4 @@
+port.user: 8000--8009
+port.movie: 8010--8019
+port.eureka-server: 8761--8769
+port.zuul: 8020--8029
